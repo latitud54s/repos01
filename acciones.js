bce32d4aebc58c7db07b16838b485b99b3926ea8
latitud54s <125929830+latitud54s@users.noreply.github.com>
2023-02-20 20:15:02 -0300

    Carga inicial

---
+++ acciones.js
@@ -0,0 +1,12 @@
+
+
+ document.getElementById('boton1').onclick = function () {      
+
+  
+
+}
+
+
+function popUp(URL) {
+    window.open(URL, 'Nombre de la ventana', 'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=1,width=400,height=300,left = 500,top = 50');
+}

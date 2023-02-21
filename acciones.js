9fc83ca04fad70743f298005a0a13c87ddf06b5f
latitud54s <125929830+latitud54s@users.noreply.github.com>
2023-02-20 21:24:19 -0300

    segunda carga
    ordenamiento

--- acciones.js
+++ acciones.js
@@ -1,12 +1,5 @@
 
 
- document.getElementById('boton1').onclick = function () {      
-
-  
-
-}
-
-
-function popUp(URL) {
-    window.open(URL, 'Nombre de la ventana', 'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=1,width=400,height=300,left = 500,top = 50');
+ function popUp(URL) {
+    window.open(URL, 'Nombre de la ventana', 'resizable=1,width=400,height=300,left = 500,top = 50');
 }
